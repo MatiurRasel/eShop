5d954f93817eb7e08e1b3e129b78041eeb15f435
MatiurRasel <matiurrasel1002@gmail.com>
2023-10-29 17:22:17 +0600

    add-changes-without-api-project

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace API.Entities
+namespace API.Controllers
 {
-    public class ProductBrand : BaseEntity
+    public class AccountController : BaseApiController
     {
-        public string Name { get; set; }
+        
     }
 }
